beb9aa2ad7a77d682afb5668aae3695e30e7784a
syh8293 <67784177+syh8293@users.noreply.github.com>
2021-10-04 18:54:16 +0900

    Create Isaiah 498-16.py

---
+++ Isaiah 498-16.py
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="ko" class="os_window">
+
+<head>
+    <title>이사야 48:16 주석</title>
+    <meta charset="utf-8">
+    <style media="screen">
+        h2 {
+            color: black;
+            text-align: center;
+        }
+
+        h3 {
+            line-height: 1.9;
+        }
+
+        p {
+            text-indent: 30px;
+            line-height: 1.8;
+            font: size 1.2em;
+        }
+
+        body {
+            background-color: whitesmoke;
+            text-align: justify;
+        }
+
+        blockquote {
+            font-size: 2;
+            color: blue;
+            line-height: 1.9;
+            border-radius: 5px;
+        }
+
+        u.spelling {
+            text-decoration: red wavy underline;
+        }
+    </style>
+
+<body>
+
+    <h2>이사야 48:16</h2>
+
+    <h3>
+        너희는 내게 가까이 나와서 이것을 들으라. 나는 처음부터 은밀한 가운데
+        말하지 않았으며, 그것이 있었던 때부터 내가 있느니라. 이제 <b>주
+            하나님</b>과 그의 <b>영</b>께서 나를
+        보내셨느니라.<sup>3</sup>
+    </h3>
+
+    <p>
+        3 (48:16) 이상한 내용 구성에 유의하라. 본문은 "주께서" 그를
+        보내셨다고 주장하나 16절의 화자는 이사야가 아니다. 왜냐하면 화자는
+        "처음부터"말했다고 주장하며 야곱을 만드신 분, 그를 부르신 분이라고
+        주장하기 때문이다 (12-15절). 그는 이사야가 아니다. 그가 누구일지라도
+        그는 신성을 주장하고 있다. 여기의 "나"는 이사야 49:3-5에서
+        밝혀진다.
+    </p>
+    <blockquote>
+        <b>12-15절</b> 내가 부른, 오 야곱 곧 이스라엘아, 내게
+        경청하라. 내가 그니라. 내가 처음이요, 내가 또한 마지막이라. 내 손이
+        땅의 기초도 놓았으며, 내 오른손이 하늘들을 폈나니 내가 그들을 부르면
+        그들이 함께 일어서느니라. 너희 모두는 모여서 들으라. 그들 가운데서 누가
+        이 일들을 선포하였느냐? 주께서 그를 사랑하셨으니 그가 바빌론에서 그의
+        기쁨을 행할 것이며, 그의 팔이 칼데아인들 위에 있으리라. 나, 곧 내가
+        말하였고, 정녕, 내가 그를 불렀으며, 그를 데려왔나니 그가 그의 길을
+        번영케 하리라.<br>
+
+        <b>이사야 49:3-5</b> 내게 말씀하시기를 “오 이스라엘아, 너는
+        나의 종이라. 네 안에서 내가 영광을 받으리라.” 하시기에 내가 말씀드리기를
+        “내가 헛되이 수고하였으며, 아무것도 아닌 것과 헛된 것을 위하여 내 힘을
+        소모했나이다.” 하였도다. 그러나 나의 심판이 분명히 주와 함께 있고, 나의
+        사역이 나의 하나님과 함께 있느니라. 이제 자기의 종을 삼으시려고 태로부터
+        나를 조성하신 주께서 야곱을 다시 자기에게로 데려오시려고 말씀하시느니라.
+        이스라엘이 모이지 않았지만 아직도 내가 주의 눈에 존귀하게 될 것이며, 내
+        하나님이 나의 힘이 되시리라.
+    </blockquote><br>
+
+    <!-- disqus 설치 코드 -->
+    <div id="disqus_thread"></div>
+    <script>
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function () { // DON'T EDIT BELOW THIS LINE
+            var d = document,
+                s = d.createElement('script');
+            s.src =
+                'https://https-syh8293-github-io-yh.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +
+                new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the
+        <a
+            href="https://disqus.com/?ref_noscript">comments
+            powered by Disqus.</a></noscript>
+    <!-- disqus  설치 코드 끝 -->
+
+</body>
+
+</html>
